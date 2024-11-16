d76ec7173e41fb3f5afc469c7a61ef5320ab7e78
Evgeniy Ivanchikay <ev.ivanchikov@gmail.com>
2024-11-16 21:02:18 +0300

    new settings for global styles

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,12 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   modules: ['@nuxtjs/strapi', '@nuxt/eslint'],
+  css: ['@/assets/styles/main.scss'],
+  app: {
+    head: {
+      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.svg' }],
+    },
+  },
   strapi: {
     url: process.env.STRAPI_URL || 'http://localhost:1337', // Default options
     prefix: '/api', // Default options
