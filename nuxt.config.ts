9dd968f02a8e2a3e9d74e971aae9c433ae36525d
Evgeniy Ivanchikay <ev.ivanchikov@gmail.com>
2024-11-26 10:41:20 +0300

    add base check strapi request

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,5 @@
+import type { AuthOptions } from '@nuxtjs/strapi';
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
@@ -16,5 +18,9 @@ export default defineNuxtConfig({
     version: 'v5', // Default options
     cookie: {}, // Default options
     cookieName: 'strapi_jwt', // Default options
+    auth: {
+      token:
+        'e4673193ee2365f48eb1ffc172621196e0125aec132670d7f039762f0b85d630 427ad468c50db93ca57f3d21547f985b1c8abb91393e24999d40fc7ff78bc9c2 2d55c3a1e3adca5c77b9b788ed87321238399a51d8b8725d66f784e5305b10f7 4d1ca48290a8b4c17f1a913771aacf56a44058bf80a9b89a5865146ceb464e4d' as string,
+    } as AuthOptions,
   },
 });
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,7 @@
+<script setup lang="ts">
+const { data } = await useStrapi().find('blogs');
+console.log(data);
+</script>
 <template>
   <div>
     <h1>Добро пожаловать на домашнюю страницу</h1>
