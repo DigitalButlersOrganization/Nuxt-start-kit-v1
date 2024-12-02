40aad91444564fca381b539498cab5a755cc39e0
Evgeniy Ivanchikay <ev.ivanchikov@gmail.com>
2024-12-02 18:23:56 +0300

    create new component RichText

---
+++ components/RichTextComponent.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+interface Props {
+  content: [];
+}
+const props = withDefaults(defineProps<Props>(), {
+  // content: any,
+});
+
+const defaultTemplate = '<div></div>';
+console.log(props.content);
+props.content.forEach((element: any) => {
+  console.log(element);
+});
+</script>
+
+<template>
+  <div v-html="defaultTemplate"></div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,65 @@
 <script setup lang="ts">
-const { data } = await useStrapi().find('blogs', {
+const { data } = (await useStrapi().find('blogs', {
   populate: '*',
-});
+})) as { data: Article[] };
 console.log(data);
+
+interface Article {
+  id: number;
+  articleName: string;
+  locale: string;
+  mainContent: string;
+}
 </script>
 <template>
-  <div>
-    <h1>Добро пожаловать на домашнюю страницу</h1>
-    <AppAlert> Это автоматически импортированный компонент. </AppAlert>
+  <h1>Добро пожаловать на домашнюю страницу</h1>
+  <AppAlert> Это автоматически импортированный компонент. </AppAlert>
+
+  <div class="example" v-for="item in data" :key="item.id">
+    <div class="example__list">
+      <div class="example__item">
+        <div class="example__item-header">
+          <h2>Имя статьи</h2>
+        </div>
+        <div class="example__item-main">
+          {{ item.articleName }}
+        </div>
+      </div>
+
+      <div class="example__item">
+        <div class="example__item-header">
+          <h2>Locale</h2>
+        </div>
+        <div class="example__item-main">
+          {{ item.locale }}
+        </div>
+      </div>
+      <div class="example__item">
+        <div class="example__item-header">
+          <h2>Main content</h2>
+        </div>
+        <div class="example__item-main">
+          {{ item.mainContent }}
+        </div>
+      </div>
+      <div class="example__item">
+        <div class="example__item-header">
+          <h2>Main content 2</h2>
+        </div>
+        <div class="example__item-main">
+          <!-- <RichTextComponent :content="item.mainContent" /> -->
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+<style scoped lang="scss">
+.example {
+  width: 100%;
+  &__example-list {
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+  }
+}
+</style>
